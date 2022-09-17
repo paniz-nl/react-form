8edf2b8ebcdd5a556ac49e1471e6ba94edb9c1cf
Paniz Nourani <paniznourani80@gmail.com>
2022-09-17 22:29:56 +0430

    changeHandler function added for input onChanges

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -10,34 +10,40 @@ const SignUp = () => {
         isAccepted: false
     })
 
-    
-
+    const changeHandler = (event) => {
+        if (event.target.name === "isAccepted") {
+            setData({...data, [event.target.name]: event.target.checked})
+        } else {
+            setData({...data, [event.target.name]: event.target.value})
+        }
+    }
+    console.log(data)
     return (
         <div>
             <form>
                 <h2>SignUp</h2>
                 <div>
                     <label>Name</label>
-                    <input type="text" name="name" value={data.name} />
+                    <input type="text" name="name" value={data.name} onChange={changeHandler} />
                 </div>
                 <div>
                     <label>Email</label>
-                    <input type="text" name="email" value={data.name} />
+                    <input type="text" name="email" value={data.email} onChange={changeHandler} />
                 </div>
                 <div>
                     <label>password</label>
-                    <input type="password" name="name" value={data.name} />
+                    <input type="password" name="password" value={data.password} onChange={changeHandler} />
                 </div>
                 <div>
                     <label>confirm Password</label>
-                    <input type="password" name="name" value={data.name} />
+                    <input type="password" name="confirmPassword" value={data.confirmPassword} onChange={changeHandler} />
                 </div>
                 <div>
                     <label>I accept terms of privacy policy</label>
-                    <input type="checkBox" name="name" value={data.name} />
+                    <input type="checkbox" name="isAccepted" value={data.isAccepted} onChange={changeHandler} />
                 </div>
-                <button>Login</button>
-                <button>Sign Up</button>
+                <a href="#">Login</a>
+                <button type="submit">Sign Up</button>
             </form>
         </div>
     );
