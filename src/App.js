b1fb7bc1834c812d83c981424f54c03cc38ceaaa
Paniz Nourani <paniznourani80@gmail.com>
2022-09-23 00:13:17 +0330

    updating react-router-dom 5 to 6

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import './App.css';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 
-import { Route, Switch, Redirect } from 'react-router-dom';
-
+//react-router-dom version 5.2.0 :
+//import { Route, Switch, Redirect } from 'react-router-dom';
+//-------------------------------------------------------------------
+//react-router-dom version 6 :
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Switch>
+      {/* <Switch>
         <Route path="/login" component={Login} />
         <Route path="/signup" component={SignUp} />
         <Redirect from="/" to="/signup" /> 
-      </Switch>
+      </Switch> */}
+
+      {/* -------------------------------------------------------------------
+      react-router-dom version 6 : */}
+      
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/" element={<Navigate to="/signup" />} />
+      </Routes>
+
+
     </div>
   );
 }
